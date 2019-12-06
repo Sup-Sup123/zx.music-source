425275fcb0ccbe3e029de67e9f4297ed7eb8b04d
itz0zkoto <53932812+itz0zkoto@users.noreply.github.com>
2019-12-06 18:39:31 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,9 @@ const { prefix } = require("../settings/config.json");
 
 module.exports.run = async (bot, message, args) => {
   let embed = new discord.RichEmbed()
-    .setTitle('Documentation')
-  .setURL('https://cadmium.glitch.me')
-  .setDescription('The Cadmium Music Bot')
-  .setAuthor('Cadmium')
-  .setThumbnail(bot.user.displayAvatarURL)
-  .setColor(bot.embedColor)
+    .setTitle("Help!")
+  .setDescription(bot.commands.map(o => `Name : **${o.help.name}** \nAliases : **${o.help.aliases}**`).join("\n")
+                 .setColor(bot.embedColor))
   message.channel.send(embed);
 };
 
